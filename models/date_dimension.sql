4fcfc5418eb0d2dd86623c05574284cb6171754a
csrao@technocratsdomain.com <csrao@technocratsdomain.com>
2026-01-14 18:31:16 +0000

    station dimension

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,21 +1,11 @@
 WITH CTE AS (
     SELECT 
-    STARTTIME::TIMESTAMP    AS START_TIME,
-    DATE(START_TIME)        AS START_DATE,
-    HOUR(START_TIME)        AS START_HOUR,
-    CASE 
-        WHEN DAYNAME(START_TIME) IN ('Sat','Sun')  THEN 'WEEKEND'
-        ELSE     'BUSINESSDAY'
-    END AS DAY_TYPE,
-    CASE   
-        WHEN MONTH(START_TIME) IN (12,1,2)  THEN 'WINTER'
-        WHEN MONTH(START_TIME) IN (3,4,5)   THEN 'SPRING'
-        WHEN MONTH(START_TIME) IN (6,7,8)   THEN 'SUMMER'
-        ELSE    'AUTUMN'
-    END AS SEASON_OF_YEAR
-
+        STARTTIME::TIMESTAMP    AS START_TIME,
+        DATE(START_TIME)        AS START_DATE,
+        HOUR(START_TIME)        AS START_HOUR,
+        {{get_daytype('START_TIME')}} AS DAY_TYPE,
+        {{get_season('START_TIME')}} AS SEASON_OF_YEAR
     FROM {{ source('DEMO', 'bike') }}
-    LIMIT 10
 )
 SELECT 
 * 
